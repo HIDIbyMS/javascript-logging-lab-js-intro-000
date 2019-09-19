57490d616ad102a8b2f1f2de7ccd44837906933e
Michelle Sharpe <hidibyms@gmail.com>
2019-09-19 20:37:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.error("EEEK!!");
-console.console.log("I will be coding!");
-console.console.warn("OH NOOO! NOT AGAIN!");
+console.log("I will be coding!");
+console.warn("OH NOOO! NOT AGAIN!");
